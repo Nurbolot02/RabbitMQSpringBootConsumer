49b5f3fba63444c3d778e5efda614859a105c030
nurbolot <ngulamidinov@gmail.com>
2023-01-19 04:41:49 +0600

    init commit

---
+++ src/main/java/com/ntg/rabbitmqspringbootconsumer/RabbitMqSpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ntg.rabbitmqspringbootconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSpringBootConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqSpringBootConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntg/rabbitmqspringbootconsumer/RabbitMqSpringBootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntg.rabbitmqspringbootconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringBootConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
